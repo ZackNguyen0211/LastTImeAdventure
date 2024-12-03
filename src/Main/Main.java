5ac3a56b9511c56a4f823cb017f73aa1c7ddcd98
Thanh Nguyễn <113710514+ZackNguyen0211@users.noreply.github.com>
2024-12-03 20:03:21 +0700

    commit Entity + Main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,27 @@
 package Main;
+import javax.swing.JFrame;
 
 public class Main {
-    
+    public static JFrame window;
+    public static void main(String[] args){
+        window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Last Time Adventure");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        gamePanel.config.loadConfig();
+        window.setUndecorated(true);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.setupGame();
+        gamePanel.startGameThread();
+
+    }
 }
